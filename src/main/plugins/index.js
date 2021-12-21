db20de83402f0d63b07e4f5bdb6e283c89b92897
Wassim <benmesswass@gmail.com>
2021-12-21 17:01:04 +0100

    commit

--- src/main/plugins/index.js
+++ src/main/plugins/index.js
@@ -1,7 +1,16 @@
+const browserify = require('@cypress/browserify-preprocessor');
 const cucumber = require('cypress-cucumber-preprocessor').default;
 /**
 * @type {Cypress.PluginConfig}
 */
 module.exports = (on, config) => {
-on('file:preprocessor', cucumber());
+/***
+* typescript
+*/
+const options = browserify.defaultOptions;
+options.typescript = process.cwd()+"/node_modules/typescript";
+/***
+* cucumber json
+*/
+on('file:preprocessor', cucumber(options));
 }
